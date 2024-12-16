6d0c44b9ed8f6990209ab1c6651dcb0611f6384a
Anuj Patel <70094088+anu581999@users.noreply.github.com>
2024-12-16 12:57:56 -0800

    Update s3_cli_command.sh

--- s3_cli_command.sh
+++ s3_cli_command.sh
@@ -3,7 +3,7 @@
 # To copy all JSON Reference data to same location:
 aws s3 cp . s3://an-on-youtube-raw-useast1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
 
-# To copy all data files to its own location, following Hive-style patterns:
+# To copy all data files to its own location:
 aws s3 cp CAvideos.csv s3://an-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ca/
 aws s3 cp DEvideos.csv s3://an-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=de/
 aws s3 cp FRvideos.csv s3://an-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=fr/
